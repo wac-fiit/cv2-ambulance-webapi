3e9b9db5a5b589de702cd6a3ef857a63f1f114c1
Tomas Bocinec <tomas.bocinec@siemens-healthineers.com>
2025-03-25 12:49:19 +0100

    Kostra servera

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/wac-fiit/cv2-ambulance-webapi/api"
+	"github.com/wac-fiit/cv2-ambulance-webapi/internal/ambulance_wl"
 )
 
 func main() {
@@ -22,6 +23,11 @@ func main() {
 	engine := gin.New()
 	engine.Use(gin.Recovery())
 	// request routings
+	handleFunctions := &ambulance_wl.ApiHandleFunctions{
+		AmbulanceConditionsAPI:  ambulance_wl.NewAmbulanceConditionsApi(),
+		AmbulanceWaitingListAPI: ambulance_wl.NewAmbulanceWaitingListApi(),
+	}
+	ambulance_wl.NewRouterWithGinEngine(engine, *handleFunctions)
 	engine.GET("/openapi", api.HandleOpenApi)
 	engine.Run(":" + port)
 }
