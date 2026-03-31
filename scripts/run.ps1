f6beaa92a4e2f7cf1f1cd3f60c4193e615018362
Michal Sevcik <michal.sevcik@protonmail.com>
2026-03-31 12:42:10 +0200

    Add dockerfile

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -35,6 +35,9 @@ switch ($command) {
     "test" {
         go test -v ./...
     }
+    "docker" {
+        docker build -t msevcik/ambulance-wl-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
     default {
         throw "Unknown command: $command"
     }
